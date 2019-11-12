a1ca845f5a348e1e6ed2ab17923d56135ac6819c
直弘 安孫子 <abi1060special@gmail.com>
2019-11-12 14:57:59 +0900

    数値入れる式でOK

--- FibonacciSequence/FibonacciSequence/Program.cs
+++ FibonacciSequence/FibonacciSequence/Program.cs
@@ -10,16 +10,17 @@ namespace FibonacciSequence
 	{
 		static void Main(string[] args)
 		{
+            int fib = int.Parse(Console.ReadLine());
 			long prev = 1;
 			long back = 0;
 			long tmp = 0;
-			for (long i = 0; i < 50; i++)
+			for (long i = 0; i <= fib; i++)
 			{
 				Console.WriteLine(prev);
 				tmp = prev;
 				prev += back;
 				back = tmp;
-
+                
 			}
 			Console.ReadKey(true);
 		}
