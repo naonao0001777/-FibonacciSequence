7ea5d8c33237a419519e44e450c21564d49f6d53
abikonao <abikonao@fsi.co.jp>
2020-01-28 14:05:56 +0900

    コメントを足しました。

--- FibonacciSequence/FibonacciSequence/Program.cs
+++ FibonacciSequence/FibonacciSequence/Program.cs
@@ -10,10 +10,13 @@ namespace FibonacciSequence
 	{
 		static void Main(string[] args)
 		{
+            // int.Parse()は文字列型をint型にするもので、
+            // Convert.ToInt16()はデータをint型にするもの
+            // Convert.ToInt16()の方が便利ではある。
             int fib = int.Parse(Console.ReadLine());
-			long prev = 1;
-			long back = 0;
-			long tmp = 0;
+			long prev = 1; // 前
+			long back = 0; // 後
+			long tmp = 0; //一時保存
 			for (long i = 0; i <= fib; i++)
 			{
 				Console.WriteLine(prev);
